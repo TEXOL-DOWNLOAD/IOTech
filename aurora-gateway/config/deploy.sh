2b68d7b9037a595e7146ca8a1b24ee6db86cc56f
Hans Chen <hans@iotechsys.com>
2023-11-27 18:12:35 +0800

    - fixed wrong picture path. - add device-mqtt service when deploy. - check influxdb status before deploy.

--- aurora-gateway/config/deploy.sh
+++ aurora-gateway/config/deploy.sh
@@ -44,30 +44,40 @@ register_appservice() {
 
 
 ## Copy resource to file system
-sudo mkdir -p /usr/share/texol
+sudo mkdir -p /usr/share/texol/picture
 sudo cp -rf picture/* /usr/share/texol/picture
 sudo cp -rf GatewayIP.txt /usr/share/texol
 
 echo "Launching Edge Xpert with the required microservices..."
-edgexpert up xpert-manager sys-mgmt device-modbus influxdb grafana texol-broker texol-ble-driver
+edgexpert up xpert-manager sys-mgmt device-modbus device-mqtt influxdb grafana texol-broker texol-ble-driver
 
 # Sleep
 sleep 10s
 
 echo "Uploading the device profile..."
+## TBD
+
+
 ##
 ## setup App-service through Xpert Manager
 ##
 API_ENDPOINT='http://localhost:9090/api/v2'
-GUI_TOKEN=$(curl --request POST --url ${API_ENDPOINT}/user/login --data '{"name":"admin","password":"admin"}')
+echo "Get Xpert Manager token by password."
+GUI_TOKEN=$(curl -s --request POST --url ${API_ENDPOINT}/user/login --data '{"name":"admin","password":"admin"}')
 
 GUI_ACCESS_TOKEN=$(echo $GUI_TOKEN | json_pp | grep access_token | cut -d'"' -f4)
-echo $GUI_ACCESS_TOKEN
+# echo $GUI_ACCESS_TOKEN
 
-## create Influxdb exporter
-POST_DATA='{"name":"influxdb-exporter","logLevel":"INFO","destination":"InfluxDB","influxDBSyncWrite":{"influxDBServerURL":"http://influxdb:8086","influxDBOrganization":"texol","influxDBBucket":"bucket","token":"texol-password","influxDBMeasurement":"readings","fieldKeyPattern":"{resourceName}","influxDBValueType":"float","influxDBPrecision":"us","authMode":"token","secretPath":"influxdb","skipVerify":"true","persistOnError":"false","storeEventTags":"false","storeReadingTags":"false"}}'
-register_appservice "$API_ENDPOINT/appSvcConf" "$GUI_ACCESS_TOKEN" "$POST_DATA"
-
+INFLUXDB_APP_NAME='"influxdb-exporter"'
+response=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $GUI_ACCESS_TOKEN" $API_ENDPOINT/appSvcConf/name/$INFLUXDB_APP_NAME)
+if [ "$response" -eq 404 ]; then
+  echo "No $INFLUXDB_APP_NAME found!, create it now"
+  ## create Influxdb exporter
+  POST_DATA='{"name":'${INFLUXDB_APP_NAME}',"logLevel":"INFO","destination":"InfluxDB","influxDBSyncWrite":{"influxDBServerURL":"http://influxdb:8086","influxDBOrganization":"texol","influxDBBucket":"bucket","token":"texol-password","influxDBMeasurement":"readings","fieldKeyPattern":"{resourceName}","influxDBValueType":"float","influxDBPrecision":"us","authMode":"token","secretPath":"influxdb","skipVerify":"true","persistOnError":"false","storeEventTags":"false","storeReadingTags":"false"}}'
+  register_appservice "$API_ENDPOINT/appSvcConf" "$GUI_ACCESS_TOKEN" "$POST_DATA"
+else
+    echo "***The $INFLUXDB_APP_NAME exsit! Please remove it and try it again"
+fi
 
 echo "Finish!"
 exit 0
